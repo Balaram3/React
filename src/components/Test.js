087dc970c45fce5ce8d0852799738ed23d9bfe14
achuri laptop <abrbalaram3@gmail.com>
2020-05-01 18:58:39 +0530

    Test and Welcome Components added

---
+++ src/components/Test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Test(){
+    return(
+        <div >
+
+        <img src='https://cdn-images-1.medium.com/max/800/1*dVfoat-JetiJaHt2DPDnzg.gif'  alt='Not Available'></img>
+        <div class="centered">Welcome to Future</div>
+        </div>
+        );
+}
+export default Test
