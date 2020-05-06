f843ca3058ef1ff33031e5bf4e8e3ff7e6c18f09
achuri laptop <abrbalaram3@gmail.com>
2020-05-06 19:10:36 +0530

    Added Key for rendering

--- src/components/ArrayRendering.js
+++ src/components/ArrayRendering.js
@@ -3,22 +3,25 @@ import ElementMap from './ElementMap'
 function ArrayRendering (){
     let arrayData=[
         {
+            id:1,
             name:'First Element',
             age:1,
             country:'India'
         },
         {
+            id:2,
             name:'Second Element',
             age:2,
             country:'US'
         },
         {
+            id:3,
             name:'Third Element',
             age:3,
             country:'SriLanka'
         }
     ]
-    let elems=arrayData.map(data=><ElementMap data={data}/>)
+    let elems=arrayData.map(data=><ElementMap key={data.id} data={data}/>)
     return(
         <div>
             {elems}
